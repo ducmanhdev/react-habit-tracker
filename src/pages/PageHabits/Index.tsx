aeb4aa16170e2ef5c855adc7ba870fc8f3e0bb21
work.ducmanh0323 <work.ducmanh0323@gmail.com>
2024-08-27 14:22:08 +0700

    Check clear current habit when list habit doesn't contain current habit

--- src/pages/PageHabits/Index.tsx
+++ src/pages/PageHabits/Index.tsx
@@ -1,7 +1,7 @@
 import {Separator} from "@/components/ui/separator.tsx"
 import LeftBar, {FilteredData} from "@/pages/PageHabits/LeftBar.tsx";
 import {useParams} from "react-router-dom";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import RightBar from "@/pages/PageHabits/RightBar.tsx";
 import HabitBoard from "@/pages/PageHabits/HabitBoard.tsx";
 import CardHabitsEmpty from "./CardHabitsEmpty.tsx";
@@ -17,13 +17,17 @@ const Index = () => {
     const {habitGroupId} = useParams();
     const [filteredHabits, setFilteredHabits] = useState<FilteredData>();
     const [currentHabit, setCurrentHabit] = useState<Doc<"habitItems">>();
-
     const habitItems = useQuery(api.habits.getHabitItems, {
         search: filteredHabits?.search,
         date: filteredHabits?.date?.valueOf(),
         order: filteredHabits?.order,
         groupId: habitGroupId as Id<"habitGroups">
     });
+
+    useEffect(() => {
+        if (habitItems?.findIndex(habit => habit._id === currentHabit?._id) !== -1) return;
+        setCurrentHabit(undefined);
+    }, [currentHabit?._id, habitItems]);
 
     const modalAddHabitIemRef = useRef<ModalAddHabitItemRef>(null);
     return (
